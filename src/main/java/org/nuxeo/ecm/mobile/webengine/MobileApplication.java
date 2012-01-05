289dd6cacd72f00f8e665c4a6cdce350c9f949f8
Benjamin JALON <bjalon@nuxeo.com>
2012-01-05 16:49:16 +0100

    NXP-8565: Add the empty webengine project and directory for webapp static

---
+++ src/main/java/org/nuxeo/ecm/mobile/webengine/MobileApplication.java
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 2006-2011 Nuxeo SAS (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Benjamin JALON <bjalon@nuxeo.com>
+ */
+
+package org.nuxeo.ecm.mobile.webengine;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.nuxeo.ecm.core.rest.DocumentRoot;
+import org.nuxeo.ecm.webengine.model.Access;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.exceptions.WebResourceNotFoundException;
+import org.nuxeo.ecm.webengine.model.exceptions.WebSecurityException;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+
+
+/**
+ * @author Benjamin JALON <bjalon@nuxeo.com>
+ */
+@Path("mobile")
+@Produces("text/html;charset=UTF-8")
+@WebObject(type="MobileApplication")
+public class MobileApplication extends ModuleRoot {
+
+    @GET
+    public Object doGet() {
+        return getView("index");
+    }
+
+}
