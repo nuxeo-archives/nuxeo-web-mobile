96d7874857996f5747b2631c361cb40da343f0d4
Benjamin JALON <bjalon@nuxeo.com>
2012-02-22 18:04:11 +0100

    NXP-8907: Add filter for Mobile Navigation Action

---
+++ nuxeo-web-mobile/src/main/seam/org/nuxeo/ecm/mobile/seam/MobileApplicationActionsBeanBean.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright ${year} Nuxeo SA (http://nuxeo.com/) and contributors.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Benjamin JALON
+ */
+
+package org.nuxeo.ecm.mobile.seam;
+
+import java.io.Serializable;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.mobile.handler.MobileRequestHandler;
+import org.nuxeo.ecm.mobile.handler.RequestHandler;
+import org.nuxeo.ecm.platform.ui.web.api.NavigationContext;
+import org.nuxeo.ecm.webapp.helpers.ResourcesAccessor;
+
+/**
+ * Actions Bean needed to build JSF actions 
+ * @author <a href="mailto:bjalon@nuxeo.com">Benjamin JALON</a>
+ *
+ */
+@Name("mobileApplicationActionsBean")
+@Scope(ScopeType.EVENT)
+public class MobileApplicationActionsBeanBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @In(create = true, required = false)
+    protected transient CoreSession documentManager;
+
+    @In(create = true)
+    protected NavigationContext navigationContext;
+
+    @In(create = true, required = false)
+    protected transient FacesMessages facesMessages;
+
+    @In(create = true)
+    protected transient ResourcesAccessor resourcesAccessor;
+
+    public boolean isMobileBrowser() {
+        RequestHandler handler = new MobileRequestHandler();
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        return handler.isRequestRedirectedToApplication(request);
+        
+        
+    }
+
+
+}
