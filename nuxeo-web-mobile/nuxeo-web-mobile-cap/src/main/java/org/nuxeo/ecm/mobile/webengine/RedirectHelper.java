e695f9c0c42e953971ce266f901da4269647f771
Florent Guillaume <fguillaume@nuxeo.com>
2015-12-26 03:11:00 +0100

    NXP-18664: add missing Nuxeo copyright headers

--- nuxeo-web-mobile/nuxeo-web-mobile-cap/src/main/java/org/nuxeo/ecm/mobile/webengine/RedirectHelper.java
+++ nuxeo-web-mobile/nuxeo-web-mobile-cap/src/main/java/org/nuxeo/ecm/mobile/webengine/RedirectHelper.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2013 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Arnaud Kervern
+ */
 package org.nuxeo.ecm.mobile.webengine;
 
 import org.apache.commons.lang.StringUtils;
@@ -13,7 +31,7 @@ import org.nuxeo.runtime.api.Framework;
 
 /**
  * Utility class to handle redirection from a given targetURL
- * 
+ *
  * @author <a href="mailto:ak@nuxeo.com">Arnaud Kervern</a>
  * @since 5.7
  */
--- nuxeo-web-mobile/nuxeo-web-mobile-cap/src/main/java/org/nuxeo/ecm/mobile/webengine/adapter/DefaultMobileAdapter.java
+++ nuxeo-web-mobile/nuxeo-web-mobile-cap/src/main/java/org/nuxeo/ecm/mobile/webengine/adapter/DefaultMobileAdapter.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2012 Nuxeo SA (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Arnaud Kervern
+ */
 package org.nuxeo.ecm.mobile.webengine.adapter;
 
 import java.util.Collections;
@@ -18,7 +36,7 @@ import org.nuxeo.ecm.webengine.model.impl.DefaultAdapter;
 
 /**
  * Default mobile adapter that exposes some usefull methods to other adapters.
- * 
+ *
  * @author <a href="mailto:akervern@nuxeo.com">Arnaud Kervern</a>
  */
 public abstract class DefaultMobileAdapter extends DefaultAdapter {
@@ -28,7 +46,7 @@ public abstract class DefaultMobileAdapter extends DefaultAdapter {
 
     /**
      * Get the current DocumentModel
-     * 
+     *
      * @return
      */
     protected DocumentModel getDocumentModel() {
@@ -41,7 +59,7 @@ public abstract class DefaultMobileAdapter extends DefaultAdapter {
 
     /**
      * Get the current MobileDocument
-     * 
+     *
      * @return
      */
     protected MobileDocument getMobileDocument() {
